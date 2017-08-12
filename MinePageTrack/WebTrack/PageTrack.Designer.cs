8dc065f67da2f733b6f1902bddbbe00c6c0862fd
Shiger <602073555@qq.com>
2017-08-12 13:39:49 +0800

    1st commit

---
+++ MinePageTrack/WebTrack/PageTrack.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WebTrack
+{
+    partial class PageTrack
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageTrack));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnTrack = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bgw_track = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_outpath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 103);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(637, 329);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnTrack
+            // 
+            this.btnTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTrack.Font = new System.Drawing.Font("楷体", 12F);
+            this.btnTrack.Location = new System.Drawing.Point(574, 34);
+            this.btnTrack.Name = "btnTrack";
+            this.btnTrack.Size = new System.Drawing.Size(75, 23);
+            this.btnTrack.TabIndex = 1;
+            this.btnTrack.Text = "爬取";
+            this.btnTrack.UseVisualStyleBackColor = true;
+            this.btnTrack.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(71, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(578, 26);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "网址：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.textBox2.Location = new System.Drawing.Point(71, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 26);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "编码：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label3.Location = new System.Drawing.Point(165, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "（gb2312，utf-8,gbk等）";
+            // 
+            // bgw_track
+            // 
+            this.bgw_track.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_track_DoWork);
+            this.bgw_track.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_track_complete);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(9, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "生成html路径：";
+            // 
+            // tb_outpath
+            // 
+            this.tb_outpath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_outpath.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.tb_outpath.Location = new System.Drawing.Point(126, 71);
+            this.tb_outpath.Name = "tb_outpath";
+            this.tb_outpath.Size = new System.Drawing.Size(523, 26);
+            this.tb_outpath.TabIndex = 16;
+            // 
+            // PageTrack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 436);
+            this.Controls.Add(this.tb_outpath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTrack);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PageTrack";
+            this.Text = "PageTrack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnTrack;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bgw_track;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_outpath;
+    }
+}
+
---
+++ MinePageTrack/WebTrack/PageTrack.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WebTrack
+{
+    public partial class PageTrack : Form
+    {
+        /****/
+        string respHtml=null;
+        /**step1**/
+        public const int Step1DataLength = 100;
+        string[] Step1SCstring = new string[Step1DataLength];
+        string[] Step1X = new string[Step1DataLength];
+        double[] Step1getJME = new double[Step1DataLength];
+        public int Step1getNum = 0;
+        string filePath = System.Environment.CurrentDirectory + "\\data";
+
+        
+
+
+        public PageTrack()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "http://www.91job.gov.cn/jobfair91/view/id/26962";
+            //step1: get html from url
+            textBox2.Text = "utf-8";
+        }
+        void track()
+        {
+            string urlToCrawl = textBox1.Text;
+            string htmlCharset =textBox2.Text;
+            //generate http request
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(urlToCrawl);
+            //use GET method to get url's html
+            req.Method = "GET";
+            //use request to get response
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+           
+            //use songtaste's html's charset GB2312 to decode html
+            //otherwise will return messy code
+            Encoding htmlEncoding = Encoding.GetEncoding(htmlCharset);
+            StreamReader sr = new StreamReader(resp.GetResponseStream(), htmlEncoding);
+            //read out the returned html
+            respHtml = sr.ReadToEnd();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnTrack.Enabled = false;
+            bgw_track.RunWorkerAsync();
+
+        }
+
+
+        private void bgw_track_DoWork(object sender, DoWorkEventArgs e)
+        {
+            track();
+            //  Step1AllOpreations();
+            string moutfile = @"./outHtml/";
+            if (!Directory.Exists(moutfile))
+            {
+                Directory.CreateDirectory(moutfile);
+            }
+
+            string mfilepath = @"./outHtml/track.html";
+            if (File.Exists(mfilepath))
+            {
+                File.Delete(mfilepath);
+            }
+            File.AppendAllText(mfilepath, respHtml);
+        }
+        private void bgw_track_complete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            richTextBox1.Text = respHtml;
+            tb_outpath.Text=  System.Environment.CurrentDirectory + "/outHtml/track.html";
+            btnTrack.Enabled = true;
+        }
+
+        public void Step1AllOpreations()
+        {
+            Step1getScode();
+
+            Step1scode2X(Step1getNum);
+
+            Step1getJMEway();
+
+            Step1SaveToFile();
+
+            for (int k = 0; k< Step1getNum; k++)
+            {
+                //Step1SCstring[j] = ExtractNum(foundSCode[j].Value);
+               //  listBox1.Items.Add(Step1X[k]);
+               //  listBox2.Items.Add(Step1X[k] + "|" + Step1SCstring[k] + "|" + DateTime.Now.Year + "|" + Step1getJME[k]);
+               //  listBox3.Items.Add(Step1X[k] + "|" + Step1SCstring[k] + "|" + Step1getWebDate() + "|" + Step1getJME[k]);
+            }
+        }
+        public void Step1SaveToFile()
+        {
+            string filePath1;
+            string filePath2;
+            string filePath3;
+            string step1FlagFile;
+            string test = Step1getWebDate();
+            /** **/
+            textBox1.Text = filePath;
+             filePath = textBox1.Text;
+             if(filePath=="")
+              {
+             MessageBox.Show("请输入文件路径！");
+                }
+             else
+             {
+             if(!Directory.Exists(filePath))
+             Directory.CreateDirectory(filePath);
+             filePath1 = filePath + "\\" +"10.txt";
+                // File.Delete(filePath1);
+                step1FlagFile = filePath + "\\" + "step1FlagFile.txt";
+                File.AppendAllText(step1FlagFile, " ");
+                StreamReader SR = new StreamReader(step1FlagFile, Encoding.Default);
+                string datelast = SR.ReadToEnd().Trim();
+                SR.Close();
+                /****/
+                if (datelast == test)
+                {
+                    MessageBox.Show(Step1getWebDate() + "日step1数据已经更新过");
+                    return;
+                }
+
+                for (int k = 0; k < Step1getNum; k++)
+             {
+                 try
+                 {
+                     File.AppendAllText(filePath1, Step1X[k] + "|" + Step1SCstring[k] + "|" + Step1getWebDate() + "|" + Step1getJME[k] + "\r\n");
+                 }
+                 catch (Exception ex)
+                 {
+                     MessageBox.Show(ex.Message);
+                 }
+             }
+             //MessageBox.Show(filePath1+"文件追加成功");
+             filePath2 = filePath + "\\"   + "2.txt";
+             File.Delete(filePath2);
+             for (int k = 0; k < Step1getNum; k++)
+             {      
+                 try
+                 {
+                     File.AppendAllText(filePath2, Step1X[k] + "|" + Step1SCstring[k] + "|" + Step1getWebDate() + "|" + Step1getJME[k] + "\r\n");
+                 }
+                 catch (Exception ex)
+                 {
+                     MessageBox.Show(ex.Message);
+                 }
+             }
+            // MessageBox.Show(filePath2 + "文件操作成功");
+             filePath3 = filePath + "\\"+"龙虎.EBK";
+             File.Delete(filePath3);
+             for (int k = 0; k < Step1getNum; k++)
+             {
+              try
+                 {
+                 File.AppendAllText(filePath3, Step1SCstring[k] + "\r\n");
+                 }
+              catch(Exception ex)
+                  {
+                     MessageBox.Show(ex.Message);
+                  }
+             }
+                File.Delete(step1FlagFile);
+                File.AppendAllText(step1FlagFile, test);
+                MessageBox.Show(Step1getWebDate() + "Step1文件操作成功");         
+        }     
+     }
+        /****/
+        public void Step1getScode()
+        {
+            /**/
+            string SCode = @"""SCode"":""(?<SCode>.+?)""";
+            MatchCollection foundSCode = (new Regex(SCode)).Matches(respHtml);
+            Step1getNum = foundSCode.Count;
+  
+            for (int j = 0; j < Step1getNum; j++)
+            {
+                Step1SCstring[j] = ExtractNum(foundSCode[j].Value);
+         //       listBox1.Items.Add(SCstring[j]);
+            }
+        }
+        /****/
+        public void Step1scode2X(int num)
+        {
+            for (int k = 0; k < num; k++)
+            {
+                // SCstring[k] = ExtractNum(foundSCode[k].Value);
+                if (string.Compare(Step1SCstring[k].Substring(0, 1), "4") < 0) Step1X[k] = "0";
+                else Step1X[k] = "1";
+                /**
+                            if (SCstring[k].Substring(0, 1) =="0") getX[k]="0";
+                            else if (SCstring[k].Substring(0, 1) == "3") getX[k] = "0";
+                            else if (SCstring[k].Substring(0, 1) == "6") getX[k] = "1";
+                **/
+               // listBox3.Items.Add(getX[k]);
+            }
+        }
+        /****/
+        public void Step1getJMEway()
+        {
+            string[] Jmstring = new string[Step1DataLength];
+            string JmMoney = @"""JmMoney"":""(?<JmMoney>.+?)""";
+            MatchCollection foundJmMoney = (new Regex(JmMoney)).Matches(respHtml);
+ 
+            /**/
+            for (int j = 0; j < foundJmMoney.Count; j++)
+            {
+                Jmstring[j] = ExtractNum(foundJmMoney[j].Value);
+            
+            }
+            /**/
+            
+            for (int k = 0; k < foundJmMoney.Count; k++)
+            {
+                Step1getJME[k] = double.Parse(Jmstring[k].Trim());
+                Step1getJME[k] = Step1getJME[k] / 10000.0;
+                Step1getJME[k] = Math.Round(Step1getJME[k], 0);
+               // listBox4.Items.Add(getJME[k]);
+            }
+        }
+        /****/
+
+        string Step1getWebDate()
+        {
+            //"Tdate":"2016-11-04"
+           // string[] Jmstring = new string[DataLength];
+            string JmMoney = @"""Tdate"":""(?<Tdate>.+?)""";
+            MatchCollection foundJmMoney = (new Regex(JmMoney)).Matches(respHtml);
+            string dataOut = ExtractDate(foundJmMoney[0].Value);
+            /**
+                    for (int i=0; i<DataLength; i++)
+                    {                 
+                        listBox2.Items.Add(foundJmMoney[i].Value);
+                    }
+            **/
+            /**
+            for (int j = 0; j < foundJmMoney.Count; j++)
+            {
+                Jmstring[j] = ExtractNum(foundJmMoney[j].Value);
+                listBox2.Items.Add(Jmstring[j]);
+            }
+            **/
+            return dataOut;
+        }
+       
+        /**/
+        string Step1getInternetDate()
+        {
+            string DateOut;
+            DateTime nowtime =  DateTime.Now;
+
+            int year = nowtime.Year;
+            int month = nowtime.Month;
+            int day = nowtime.Day;
+            string Ystring=year.ToString();
+            string Mstring=month.ToString();
+            string Dstring=day.ToString();
+            
+            if (month < 10) Mstring = "0" + Mstring;
+            if (day < 10)   Dstring = "0" + Dstring;
+
+            DateOut = Ystring+Mstring+Dstring;
+
+            return DateOut;
+        }
+        /**/
+        public string ExtractNum(string sourse)
+        {
+            string strSplit2 = Regex.Replace(sourse, "[A-z]", "", RegexOptions.IgnoreCase);
+            strSplit2 = Regex.Replace(strSplit2, "[:]", "", RegexOptions.IgnoreCase);
+            strSplit2 = Regex.Replace(strSplit2, "[\"]", "", RegexOptions.IgnoreCase);
+            return strSplit2;
+        }
+
+        public string ExtractDate(string sourse)
+        {
+            string strSplit2 = Regex.Replace(sourse, "[A-z]", "", RegexOptions.IgnoreCase);
+            strSplit2 = Regex.Replace(strSplit2, "[:]", "", RegexOptions.IgnoreCase);
+            strSplit2 = Regex.Replace(strSplit2, "[\"]", "", RegexOptions.IgnoreCase);
+            strSplit2 = Regex.Replace(strSplit2, "-", "", RegexOptions.IgnoreCase);
+            return strSplit2;
+        }
+
+    }
+}
